cb6dc2a337884fd899c63dffa12b68e5ca273569
dasasdhba <dasasdhba@outlook.com>
2026-07-04 12:57:32 +0800

    rework result partition

--- FSharp.Extend/Extend/Seq.fs
+++ FSharp.Extend/Extend/Seq.fs
@@ -1,5 +1,28 @@
 module FSharp.Extend.Seq
 
+let partition predicate source =
+    let yes = ResizeArray<'a>()
+    let no = ResizeArray<'a>()
+
+    for x in source do
+        if predicate x then
+            yes.Add x
+        else
+            no.Add x
+
+    yes, no
+
+let partitionResult source =
+    let yes = ResizeArray<'a>()
+    let no = ResizeArray<'b>()
+
+    for x in source do
+        match x with
+        | Ok x -> yes.Add x
+        | Error x -> no.Add x
+
+    yes, no
+
 let tryMinBy projection (source : 'a seq) =
     use e = source.GetEnumerator()
 
